54cd73ee1c5db6a38bb6a6b84c36819f62007d5c
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-08 18:58:27 -0500

    Resolucion del ejercicio 34 sobre email

--- prueba34.py
+++ prueba34.py
@@ -1,14 +1,13 @@
 '''Crea un programa con un bucle for y una sentencia break. El programa debe iterar sobre los caracteres en una dirección de correo electrónico, salir del bucle cuando llegue al símbolo @ e imprimir la parte antes de @ en una línea. Usa el esqueleto de abajo:'''
-without = ""
-for ch in "john.smith@pythoninstitute.org":
-    without = without + ch
-    if ch == "@":
-        break
-print(without)
 
-'''Codigo sugerido en el curso
 for ch in "john.smith@pythoninstitute.org":
     if ch == "@":
         break
     print(ch, end="")
-'''
+
+'''without = ""
+for ch in "john.smith@pythoninstitute.org":
+    without = without + ch
+    if ch == "@":
+        break
+print(without)'''
