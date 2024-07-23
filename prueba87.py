84c79430bbcd8b5fc7119060cd5dae949d47e8ad
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-23 18:47:57 -0500

    Aplicaciones avanzadas con listas Pt.2

---
+++ prueba87.py
@@ -0,0 +1,15 @@
+'''Ahora encuentra la temperatura más alta durante todo el mes'''
+temps = [[0.0 for h in range(24)] for d in range(31)]
+#
+# La matriz se actualiza aquí.
+#
+
+highest = -100.0
+
+for day in temps:
+    for temp in day:
+        if temp > highest:
+            highest = temp
+
+print("La temperatura más alta fue:", highest)
+
