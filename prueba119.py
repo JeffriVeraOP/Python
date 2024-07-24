5b4cd1f8cc8a726495cbe05434d4768179052775
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-24 14:30:42 -0500

    Funciones Seccion 2 Quiz 4

--- prueba119.py
+++ prueba119.py
@@ -1,6 +1,6 @@
-def add_numbers(a, b=2, c):
+'''def add_numbers(a, b=2, c):
     print(a + b + c)
 
-add_numbers(a=1, c=3)
+add_numbers(a=1, c=3)'''
 
 # SyntaxError - un argumento no-predeterminado (c) sigue a un argumento predeterminado (b=2).
