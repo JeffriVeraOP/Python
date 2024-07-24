03c4734b427c5b18343563567bbbb276d8bec74d
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-24 13:56:56 -0500

    Paso de parametros posicionales Ejemplos

---
+++ prueba111.py
@@ -0,0 +1,11 @@
+def introduction(first_name, last_name):
+    print("Hola, mi nombre es", first_name, last_name)
+
+introduction("Luke", "Skywalker")
+introduction("Jesse", "Quick")
+introduction("Clark", "Kent")
+
+introduction("Skywalker", "Luke")
+introduction("Quick", "Jesse")
+introduction("Kent", "Clark")
+
