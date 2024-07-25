098cc93fcac77118e96970726d873e1c5d5f89ff
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-25 13:31:42 -0500

    LAB 2 Funciones Dias del mes/año bisiesto o no

--- prueba123.py
+++ prueba123.py
@@ -3,7 +3,7 @@ La parte inicial de la función está lista. Ahora, haz que la función devuelva
 Por supuesto, puedes (y debes) utilizar la función previamente escrita y probada (LAB 4.3.1.6). Puede ser muy útil. Te recomendamos que utilices una lista con los meses. Puedes crearla dentro de la función - este truco acortará significativamente el código.
 Hemos preparado un código de prueba. Amplíalo para incluir más casos de prueba.'''
 
-def is_year_leap(year):
+def is_year_leap(yr):
     if yr % 4 != 0:
         return False
     elif yr % 100 != 0:
@@ -13,17 +13,17 @@ def is_year_leap(year):
     else:
         return True
 
-def days_in_month(year, month):
-    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    days_month_n = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    days_month_b = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    return
-    if is_year_leap(year):
-        for i in months:
-            month = days_month_n[i]
+def days_in_month(yr, mo):
+    days_in_month_b = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_in_month_n = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_year_leap(yr) == True:
+        result = days_in_month_b[mo-1]
+        return result
+    elif is_year_leap(yr) == False:
+        result = days_in_month_n[mo-1]
+        return result
     else:
-        for i in months:
-            month = days_month_b[i]    
+        return None
 
 test_years = [1900, 2000, 2016, 1987]
 test_months = [2, 2, 1, 11]
@@ -37,3 +37,15 @@ for i in range(len(test_years)):
         print("OK")
     else:
         print("Fallido")
+# OK OK OK OK
+
+'''Solucion de Muestra de CISCO
+def days_in_month(year,month):
+    if year < 1582 or month < 1 or month > 12:
+        return None
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    res  = days[month - 1]
+    if month == 2 and is_year_leap(year):
+        res = 29
+    return res
+'''
