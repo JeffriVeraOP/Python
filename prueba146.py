ad66788f5e30ee5749f70b3336e1f7e6351ff2a6
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-28 18:57:09 -0500

    Diccionarios Alineacion Vertical o Sangria Francesa

---
+++ prueba146.py
@@ -0,0 +1,11 @@
+# Ejemplo 1:
+dictionary = {
+              "gato": "chat",
+              "perro": "chien",
+              "caballo": "cheval"
+}
+# Ejemplo 2:
+phone_numbers = {'jefe': 5551234567,
+              'Suzy': 22657854310
+}
+
