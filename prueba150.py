07b8a3094e5387aa33791fc14898ac81e7469798
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-28 19:22:57 -0500

    Diccionarios y .sorted()

---
+++ prueba150.py
@@ -0,0 +1,7 @@
+dictionary = {"gato": "chat", "perro": "chien", "caballo": "cheval"}
+keys_dictionary = []
+
+for key in sorted(dictionary.keys()):
+    keys_dictionary.append(key)
+    
+print(keys_dictionary)
