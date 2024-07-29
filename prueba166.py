6e1b8bdd17b6c05e804f32da95b2991365ed5b58
JeffriVeraOP <jeffveratavara@gmail.com>
2024-07-28 22:39:41 -0500

    Tuplas y diccionarios Quiz 6

---
+++ prueba166.py
@@ -0,0 +1,14 @@
+colors = (("green", "#008000"), ("blue", "#0000FF"))
+
+colors_dictionary = {}
+
+for key, value in colors:
+    colors_dictionary[key] = value
+
+print(colors_dictionary)
+
+''' Cisco pone como respuesta un dict() aunque no lo enseño en la teoria:
+
+colors_dictionary = dict(colors)
+
+'''
