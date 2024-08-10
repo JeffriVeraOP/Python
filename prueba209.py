9fced99a4c1964ee2822d3e64b7dd19468968a67
JeffriVeraOP <jeffveratavara@gmail.com>
2024-08-10 11:01:08 -0500

    Prueba Final Pregunta 31

---
+++ prueba209.py
@@ -0,0 +1,12 @@
+try:
+    value = input("Ingresa un valor: ")
+    print(int(value)/len(value))
+except ValueError:
+    print("Entrada incorrecta...")
+except ZeroDivisionError:
+    print("Entrada erronea...")
+except TypeError:
+    print("Entrada muy erronea...")
+except:
+    print("¡Buuu!")
+
